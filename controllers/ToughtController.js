44bcd149534aca9d7d9748d4e5518638c6e54a34
leomazzeu <leonardomazzeudasilva@gmail.com>
2021-12-28 08:50:50 -0300

    Resgatando pensamentos no Dashboard

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -8,7 +8,20 @@ module.exports = class ToughtController {
   }
 
   static async dashboard(req, res) {
-    res.render('toughts/dashboard')
+    const UserId = req.session.userid
+
+    const user = await User.findByPk(UserId, { include: Tought, plain: true })
+
+    // check user exists
+    if(!user) {
+      res.redirect('/login')
+    }
+
+    const toughts = user.Toughts.map((result) => result.dataValues)
+    console.log(toughts)
+
+
+    res.render('toughts/dashboard', { toughts })
   }
 
   static createTought(req, res) {
