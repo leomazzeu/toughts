77ac9c25dbb955e9afd140d2d47aa85517f20c8d
leomazzeu <leonardomazzeudasilva@gmail.com>
2021-12-27 21:14:06 -0300

    Registrando usuário e mantendo sessão

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -24,5 +24,35 @@ module.exports = class AuthController {
       return
     }
 
+    // check if user exists
+    const checkUserExists = await User.findOne({ where: { email } })
+    if(checkUserExists) {
+      req.flash('message', 'Este e-mail já está em uso!')
+      res.render('auth/register')
+
+      return
+    }
+
+    //create a password
+    const salt = bcrypt.genSaltSync(10) // Coloca 10 caracteres randômicos na senha
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+    const user = {name, email, password: hashedPassword}
+
+    try {
+      const createdUser = await User.create(user)
+
+      // initialize session
+      req.session.userid = createdUser.id
+
+      req.flash('message', 'Cadastro realizado com sucesso!')
+
+      req.session.save(() => {
+        res.redirect('/')
+      })
+      res.redirect('/')
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
